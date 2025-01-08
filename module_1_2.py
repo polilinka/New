73a9fc60e601263c23120a41e617b64ed4ad7132
polilinka <73548939+polilinka@users.noreply.github.com>
2025-01-08 16:40:13 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+number_of_homeworks = 12
+number_of_hours = 1.5
+course_name = 'Python'
+hours_for_one_homework = number_of_hours/number_of_homeworks
+print('Курс: ', course_name,',', 'всего задач: ', number_of_homeworks,',', 'затрачено часов:', number_of_hours,',', 'среднее время выполнения:', hours_for_one_homework)
