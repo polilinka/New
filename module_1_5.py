cb7051007b956d559b127205cb3269de2aa6d449
polilinka <73548939+polilinka@users.noreply.github.com>
2025-01-22 20:50:11 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (13, 15, 'hello', 3.2)
+print(immutable_var)
+
+#immutable_var[2] = 17
+
+mutable_list = [1, 2, 3, 'hi', True, 1.2]
+print(mutable_list)
+mutable_list[1] = 'hello'
+print(mutable_list)
